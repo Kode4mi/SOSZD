594ad91844f8ded092819ee77f986fc3244e3df0
Michał Szajner <michalszajner@wp.pl>
2022-10-27 16:12:58 +0200

    Poprawki wyglądu Navbara

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -1,11 +1,23 @@
 <div class="navbar">
     <div class="navbar__logo">
-        <span class="navbar__nazwa_1">so</span><span class="navbar__nazwa_2">SZD</span>
+        <div class="navbar__logo--img">
+            <span class="navbar__nazwa_1">so</span><span class="navbar__nazwa_2">SZD</span>
+        </div>
     </div>
     <div class="navbar__sidebar">
-        <p> <input type="button" value="Aktualności" class="navbar__sidebar--button navbar__sidebar--button1" />   </p>
-        <p> <input type="button" value="Nowa Sprawa"  class="navbar__sidebar--button" />  </p>
-        <p> <input type="button" value="Użytkownicy"  class="navbar__sidebar--button" />  </p>
-        <p> <input type="button" value="Wyloguj"  class="navbar__sidebar--button" />      </p>
+        <ul>
+            <li>
+                <button class="navbar__sidebar--button navbar__sidebar--button1">Aktualności</button>
+            </li>
+            <li>
+                <button class="navbar__sidebar--button">Nowa Sprawa</button>
+            </li>
+            <li>
+                <button class="navbar__sidebar--button">Użytkownicy</button>
+            </li>
+            <li>
+                <button class="navbar__sidebar--button">Wyloguj</button>
+            </li>
+        </ul>
     </div>
 </div>

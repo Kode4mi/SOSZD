7b782f1a7536cfbfe608a347e13c1e5583dca5dd
Michał Szajner <michalszajner@wp.pl>
2022-10-27 09:18:02 +0200

    Zmiany układu plików navbara

---
+++ resources/views/partials/_navbar.blade.php
@@ -0,0 +1,11 @@
+<div class="navbar">
+    <div class="navbar__logo">
+        <span class="navbar__nazwa_1">so</span><span class="navbar__nazwa_2">SZD</span>
+    </div>
+    <div class="navbar__sidebar">
+        <p> <input type="button" value="Aktualności" class="navbar__sidebar--button navbar__sidebar--button1" />   </p>
+        <p> <input type="button" value="Nowa Sprawa"  class="navbar__sidebar--button" />  </p>
+        <p> <input type="button" value="Użytkownicy"  class="navbar__sidebar--button" />  </p>
+        <p> <input type="button" value="Wyloguj"  class="navbar__sidebar--button" />      </p>
+    </div>
+</div>
--- resources/views/templates/layout.blade.php
+++ resources/views/templates/layout.blade.php
@@ -10,22 +10,14 @@
     <script src="{{ asset('js/app.js') }}"></script>
 </head>
 <body>
-    <div class="navbar">
-    <div class="navbar__logo">
-    <span class="navbar__nazwa_1">so</span><span class="navbar__nazwa_2">SZD</span>
-    </div>
-    <div class="navbar__sidebar"> 
-       <p> <input type="button" value="Aktualności" class="navbar__sidebar--button navbar__sidebar--button1" />   </p>
-       <p> <input type="button" value="Nowa Sprawa"  class="navbar__sidebar--button" />  </p>
-       <p> <input type="button" value="Użytkownicy"  class="navbar__sidebar--button" />  </p>
-       <p> <input type="button" value="Wyloguj"  class="navbar__sidebar--button" />      </p>
-    </div>
-    </div>
-    <div class="header"></div>
-    <div class="main"></div>
 
-<div>
+@include('partials._navbar')
+
+<div class="header"></div>
+<div class="main">
+
     @yield('content')
+
 </div>
 </body>
 </html>

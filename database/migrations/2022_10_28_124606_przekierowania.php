605b4d72511b3767adfb7d5205dd9e067540b48f
Deznka <jan.slepaczuk@gmail.com>
2022-10-28 15:31:19 +0200

    Poprawka do tabeli redirects

--- database/migrations/2022_10_28_124606_przekierowania.php
+++ database/migrations/2022_10_28_124606_przekierowania.php
@@ -38,6 +38,6 @@ return new class extends Migration
         //     $table->dropForeign(['users_id']);
         // });
         
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('redirects');
     }
 };

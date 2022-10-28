7c58ace6f4257178c83856c8b4004e26d0d4b248
Deznka <jan.slepaczuk@gmail.com>
2022-10-28 14:59:41 +0200

    poprawa tabeli users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', static function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('position');
+            $table->string('email');
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
         });
     }
 

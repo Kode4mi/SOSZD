a52991b6984c7bca313468a88fd53b19dc7c4f39
KotKamil <62114754+KotKamil@users.noreply.github.com>
2022-11-11 17:05:01 +0100

    Dodano stylowanie strony /forget-password

--- resources/views/user/forget-password.blade.php
+++ resources/views/user/forget-password.blade.php
@@ -2,22 +2,38 @@
 
 @section('cont')
 
-    <main>
+    <main class="login">
 
-        <form method="POST" action="/forget-password" class="w-full text-center mt-5">
+        <form method="POST" action="/forget-password" class="login__form login__form--forgetpassword">
             @csrf
             @method('POST')
-            <p>
-                <label for="email">Podaj email twojego konta: </label>
-                <input type="text" value="" id="email" name="email">
-            </p>
 
-            @error('email')
-            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
+            <div class="login__center">
+                <div class="login__iconbox">
+                    <div class="login__largefont login__icon"><i class="fa-solid fa-a fa-3x"></i></div>
+                    <div class="login__smallfont login__icon"><i class="fa-solid fa-a fa-2xs"></i></div>
+                    <div class="login__contrast login__icon" ><i class="fa-solid fa-circle-half-stroke fa-3x" onClick="contrastToggle()"></i></div>
+                </div>
+            </div>
+            <div class="login__center">
+                <div class="login__logo">
+                    <img src="{{asset('images/logo-lepsze.png')}}" alt="logo">
+                </div>
+            </div>
+
             <p>
-                <button type="submit">Wyślij email zmiany hasła</button>
+                <input class="login__input" placeholder="Podaj email twojego konta:" type="text" value="" id="email" name="email">
             </p>
+           
+            <p>
+                <button type="submit" class="login__button login__button--forgetpassword">Wyślij email zmiany hasła</button>
+            </p>
+            
+            <div class="login__errorpanel">
+                @error('email')
+                <p class="login__errormessage">{{$message}}</p>
+                @enderror
+            </div>
         </form>
 
     </main>

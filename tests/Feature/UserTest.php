22dd820f248025c901818f78d3c1ef7f755e3cc5
Deznka <jan.slepaczuk@gmail.com>
2022-11-12 00:56:48 +0100

    Dodanie częsci testów usera | LOGOWANIE DO NAPRAWY

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -10,6 +10,17 @@ use Tests\TestCase;
 
 class UserTest extends TestCase
 {
+    /*
+        Do zrobienia:
+
+        login 
+        authenticate
+        logout
+        update - na późńiej
+        update_pass
+    */
+
+
     public function test_user_edit_status(): void
     {
         $user = User::factory()->make();
@@ -18,6 +29,76 @@ class UserTest extends TestCase
 
         $response->assertSuccessful();
         $response->assertViewIs('user.edit'); // bez kropki wypierdala błąd :)
+    }
+
+    public function test_user_edit_status_not_auth(): void
+    {
+        $response = $this->get('/user/edit');
+
+        $response->assertRedirect('login');
+    }
+
+    public function test_user_editPassword_status(): void
+    {
+        $user = User::factory()->make();
+
+        $response = $this->actingAs($user)->get('/change-password');
+
+        $response->assertSuccessful();
+        $response->assertViewIs('user.password-change');
+    }
+    public function test_user_editPassword_status_not_auth(): void
+    {
+        $response = $this->get('/change-password');
+
+        $response->assertRedirect('login');
+    }
+
+    public function test_user_index_status(): void
+    {
+        $user = User::factory()->create([
+            'first_name' => "Kunegunda",
+            'role' => 'admin'
+        ]);
+
+        $response = $this->actingAs($user)->get('/users');
+
+        $response->assertSuccessful();
+        $response->assertViewIs('user.index','');
+        $user->delete();
+    }
+    public function test_user_index_status_not_auth(): void
+    {
+        $response = $this->get('/users');
+
+        $response->assertRedirect('login');
+    }
+
+    public function test_user_authenticate_status(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post('/login',[
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+        
+        // albo z emailem albo z hasłem jest coś zjebane, bo jakby nie logowało 
+
+        $response->assertRedirect('tickets');
+        $user->delete();
+    }
+
+    public function test_user_authenticate_status_wrong_pass(): void
+    {
+        $response = $this->post('/login',[
+            'email' => 'twojstary@soszd.pl',
+            'password' => "2"
+        ]);
+        
+        // albo z emailem albo z hasłem jest coś zjebane - tu to wogóle szkoda strzępić ryja
+
+        $response->assertRedirect('login');
     }
 
 }

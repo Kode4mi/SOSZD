22639c7e350e8f1fae73e343a233e075d0dfe239
KotKamil <62114754+KotKamil@users.noreply.github.com>
2022-11-21 20:47:42 +0100

    Poprawa wyświetlania tabeli użytkowników

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -23,14 +23,25 @@
                 <th>@sortablelink('last_name', 'Nazwisko')</th>-->
             <th class="users__header-link">@sortablelink('role', 'Stanowisko')</th>    
             <th class="users__header-link">@sortablelink('email', 'E-mail')</th>
+            <th class="users__select-sorter">
+                        <label for="select-sort" class="users__select-sorter-label">Sortuj po:</label>
+                        <select name="select-sort" id="select-sort">
+                            <option value="a">Dane (Rosnąco)</option>
+                            <option value="s">Dane (Malejąco)</option>
+                            <option value="d">Rola (Rosnąco)</option>
+                            <option value="f">Rola (Malejąco)</option>
+                            <option value="g">E-mail (Rosnąco)</option>
+                            <option value="h">E-mail (Malejąco)</option>
+                        </select>
+                    </th>
         </tr>
     </thead>
     <tbody>
         @foreach($users as $users)
         <tr class="users__row">
-            <td>{{$users->last_name}} {{$users->first_name}}</td>
-            <td>{{$users->role}}</td>
-            <td>{{$users->email}}</td>
+            <td class="users__name">{{$users->last_name}} {{$users->first_name}}</td>
+            <td class="users__role">{{$users->role}}</td>
+            <td class="users__email">{{$users->email}}</td>
         </tr>
         @endforeach
     </tbody>

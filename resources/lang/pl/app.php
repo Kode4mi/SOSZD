4970955f80dc0cf96022be9d29219de6a1f3f084
Michał Szajner <michalszajner@wp.pl>
2022-11-22 13:46:46 +0100

    Zmiana wyświetlania daty w kolumnie termin

--- resources/lang/pl/app.php
+++ resources/lang/pl/app.php
@@ -35,6 +35,21 @@ return [
     'first_name' => 'imie',
     'last_name' => 'nazwisko',
 
+    'month' => [
+        '1' => "styczeń",
+        '2' => "luty",
+        '3' => "marzec",
+        '4' => "kwiecień",
+        '5' => "maj",
+        '6' => "czerwiec",
+        '7' => "lipiec",
+        '8' => "sierpień",
+        '9' => "wrzesień",
+        '10' => "październik",
+        '11' => "listopad",
+        '12' => "grudzień",
+    ],
+
 
     // SOSZD
 

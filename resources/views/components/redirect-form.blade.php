f883e9f737d835e51dca5650f78c6a4220d3a6a4
Michał Szajner <michalszajner@wp.pl>
2022-12-16 18:17:20 +0100

    Wyszukiwanie użytkownika w przysyłaniu (na razie przesłanie nie działa)

--- resources/views/components/redirect-form.blade.php
+++ resources/views/components/redirect-form.blade.php
@@ -3,12 +3,45 @@
     @csrf
     @method("POST")
 
+    <label>
+        <input oninput="updateResult(this.value)"  type="text" placeholder="Szukaj Adresata">
+    </label>
+
     <label class="label_adresat" for="user_select">Adresaci:</label>
-    <select class="redirect-form__select" name="user_id[]" multiple id="user_select">
+    <ul class="redirect-form__select" multiple id="user_list">
         @foreach($users as $user)
-            <option class="redirect-form__option" value="{{$user->id}}"> {{$user->first_name}} {{$user->last_name}} </option>
+            <li class="redirect-form__option" value="{{$user->id}}"> {{$user->first_name}} {{$user->last_name}} </li>
         @endforeach
-    </select>
+    </ul>
+
+    <script>
+
+        let arr = document.getElementsByClassName("redirect-form__option");
+
+        let list = [];
+
+        for(let i=0; i< arr.length; i++) {
+            list[i] = "";
+            list[i] += arr.item(i).textContent;
+        }
+
+        console.log(list);
+
+        function updateResult(query) {
+            let resultList = document.getElementById("user_list");
+            resultList.innerHTML = "";
+
+            list.map(function(algo){
+                query.split(" ").map(function (word){
+                    if(algo.toLowerCase().indexOf(word.toLowerCase()) !== -1){
+                        resultList.innerHTML += `<li class="redirect-form__option">${algo}</li>`;
+                    }
+                })
+            })
+        }
+
+    </script>
+
 
     @error('user_id')
     <p>{{$message}}</p>

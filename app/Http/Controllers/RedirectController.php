8983bd60079f5e65bd30361029f8c1a0da816ebe
Michał Szajner <michalszajner@wp.pl>
2023-01-05 13:10:49 +0100

    Naprawa przesyłania sprawy, wyszukiwanie i aktualizacja testów przesyłania

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -31,7 +31,9 @@ class RedirectController extends Controller
             'user_id' => __('app.user_id')
         ]);
 
-        foreach ($formFields['user_id'] as $user) {
+        $userTab = explode(",", $formFields['user_id']);
+
+        foreach ($userTab as $user) {
             $redirect = ['user_id' => $user, 'ticket_id' => $ticket->id];
             Redirect::create($redirect);
         }
--- resources/views/components/redirect-form.blade.php
+++ resources/views/components/redirect-form.blade.php
@@ -4,41 +4,59 @@
     @method("POST")
 
     <label>
-        <input oninput="updateResult(this.value)"  type="text" placeholder="Szukaj Adresata">
+        <input class="user-input" oninput="updateResult(this.value)" type="text" placeholder="Szukaj Adresata" />
     </label>
+
+    <label>Wybrani użytkownicy:</label>
+    <p class="selected-list"></p>
+
 
     <label class="label_adresat" for="user_select">Adresaci:</label>
     <ul class="redirect-form__select" multiple id="user_list">
         @foreach($users as $user)
-            <li class="redirect-form__option" value="{{$user->id}}"> {{$user->first_name}} {{$user->last_name}} </li>
+            <li class="redirect-form__option" value="{{$user->id}}"
+                onclick="addToSelected({{$user->id}})"> {{$user->first_name}} {{$user->last_name}} </li>
         @endforeach
     </ul>
 
-    <script>
+    <script type="text/javascript">
 
         let arr = document.getElementsByClassName("redirect-form__option");
-
+        let selected = [];
+        let sliced = [];
         let list = [];
 
-        for(let i=0; i< arr.length; i++) {
-            list[i] = "";
-            list[i] += arr.item(i).textContent;
+        for (let i = 0; i < arr.length; i++) {
+            list.push({name: arr.item(i).textContent, id: arr.item(i).value});
         }
 
-        console.log(list);
-
-        function updateResult(query) {
+        const updateResult = (query) => {
             let resultList = document.getElementById("user_list");
             resultList.innerHTML = "";
 
-            list.map(function(algo){
-                query.split(" ").map(function (word){
-                    if(algo.toLowerCase().indexOf(word.toLowerCase()) !== -1){
-                        resultList.innerHTML += `<li class="redirect-form__option">${algo}</li>`;
+            list.map((user) => {
+                query.split(" ").map(function (word) {
+                    if (user.name.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
+                        resultList.innerHTML += `<li class="redirect-form__option" onclick="addToSelected(${user.id})">${user.name}</li>`;
                     }
                 })
             })
         }
+
+        const addToSelected = (id) => {
+            selected.push(id);
+            const index = list.map(user => user.id).indexOf(id);
+            sliced.push(list.splice(index, 1));
+
+            document.querySelector(".input-selected").innerHTML = `<input type="hidden" value="${selected}" name="user_id"/>`;
+
+            updateResult(document.querySelector(".user-input").value);
+
+            document.querySelector(".selected-list").innerHTML = sliced.map((item) => {
+                return item[0].name;
+            });
+        }
+
 
     </script>
 
@@ -46,6 +64,10 @@
     @error('user_id')
     <p>{{$message}}</p>
     @enderror
+
+    <span class="input-selected">
+        <input type="hidden" value="" name="user_id"/>
+    </span>
 
     <button class="redirect-form__button" type="submit">Prześlij</button>
 

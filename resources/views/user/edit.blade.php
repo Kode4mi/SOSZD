970674af37a4a402dd716f202979f417ca1889ed
qspyx <mikolajszokaluk@gmail.com>
2023-01-11 12:13:56 +0100

    zmiany w ukladzie edit blade ale to ndziala

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -13,36 +13,20 @@
 
             <h4>Zmień e-mail:</h4>
             <input type="text" class="user__input" placeholder="E-mail" name="email" value="{{auth()->user()->email}}"> <br />
-            <input type="password" class="user__input" placeholder="Wpisz hasło aby zmienić e-mail" name="password_confirmation">
-            <button type="submit" class="user__submit">Zatwierdź e-mail</button>
-
-         
-
+            
             @error('email')
             <p>{{$message}}</p>
             @enderror
+
+            <input type="password" class="user__input" placeholder="Wpisz hasło aby zmienić e-mail" name="password_confirmation">
             
-  
-
-            <hr />
-<!--
-            <h4>Zmień hasło</h4>
-            
-            <input type="password" class="user__input" placeholder="Stare hasło" name="password">
-
-            @error('password')
-            <p>{{$message}}</p>
-            @enderror <br/>
-
-            <input type="password" class="user__input" placeholder="Nowe hasło" name="password_confirmation">
-
             @error('password_confirmation')
             <p>{{$message}}</p>
             @enderror
 
-        <button type="submit" class="user__submit">Zatwierdź hasło</button>
+            <button type="submit" class="user__submit">Zatwierdź e-mail</button>
+            <hr />
 
-        !-->
             <p class="mt-2"><a class="main-window__a" href="/change-password">Zmień hasło</a></p> 
 
         </form>
